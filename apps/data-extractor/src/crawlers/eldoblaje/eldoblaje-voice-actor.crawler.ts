998a5cad9dad30228e4b6d2690f82e3d4f4a15ef
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2024-03-19 18:42:23 +0100

    feat: add voice actor crawler

--- apps/data-extractor/src/crawlers/eldoblaje/eldoblaje-voice-actor.crawler.ts
+++ apps/data-extractor/src/crawlers/eldoblaje/eldoblaje-voice-actor.crawler.ts
@@ -1,7 +1,46 @@
 import { CheerioCrawlingContext } from 'crawlee'
+import { extractRowInfo, formatActorName } from './eldoblaje-common.utils'
+import { CheerioAPI } from 'cheerio'
+import { VoiceActor } from '../movie.types'
 
-export function extractVoiceActorData({ _request, _ }: CheerioCrawlingContext) {
-  return undefined
+export function extractVoiceActorData(crawlingContext: CheerioCrawlingContext): VoiceActor {
+  const { request, $ } = crawlingContext
+  const url = new URL(request.url)
+  const sourceId = url.searchParams.get('id')
+
+  const name = extractBasicInfo($, 'Nombre:') || ''
+  const recurringVoiceOf = extractRecurringVoice($).map(formatActorName)
+
+  if (!name || !sourceId) throw new Error('Could not extract voice actor data')
+
+  const tempId = getVoiceActorId(sourceId)
+
+  return {
+    id: tempId,
+    name,
+    recurringVoiceOf,
+    language: 'es-ES',
+    sourceId,
+  }
+}
+
+// Is this really the most clean way of doing this? Seems awkward
+// This is only valid for the information on the "Ficha elDoblaje"
+function extractBasicInfo($: CheerioAPI, regexOrString: RegExp | string) {
+  return extractRowInfo($, 'td > span.trebuchett', regexOrString)
+}
+
+// Is this really the most clean way of doing this? Seems awkward
+export function extractRecurringVoice($: CheerioAPI) {
+  const recurringVoices = $('.arial16white')
+    .filter(function () {
+      return $(this).text().includes('Voces Habituales en Doblaje')
+    })
+    .first()
+    .parentsUntil('tbody')
+    .find('a')
+
+  return recurringVoices.map((_, el) => $(el).text().trim()).toArray()
 }
 
 export function getVoiceActorId(sourceId: string) {
