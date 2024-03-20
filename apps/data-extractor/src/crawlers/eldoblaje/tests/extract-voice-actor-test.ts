f05773a52a13f09b6946730ddf45190f6245ad18
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2024-03-21 00:30:01 +0100

    feat: add voice actor test

---
+++ apps/data-extractor/src/crawlers/eldoblaje/tests/extract-voice-actor-test.ts
@@ -0,0 +1,23 @@
+import { CheerioCrawler } from "crawlee";
+import { extractVoiceActorData } from "../eldoblaje-voice-actor.crawler";
+
+const testVoiceActorUrl =
+  "https://www.eldoblaje.com/datos/FichaActorDoblaje.asp?id=276";
+
+export async function extractVoiceActorTest() {
+  const crawler = new CheerioCrawler({
+    forceResponseEncoding: "iso-8859-1",
+    async requestHandler(crawlingContext) {
+      const urlData = new URL(crawlingContext.request.url);
+      if (urlData.pathname !== "/datos/FichaActorDoblaje.asp") {
+        return undefined;
+      }
+
+      const data = extractVoiceActorData(crawlingContext);
+      console.info("Voice actor extractor result");
+      console.dir(data, { depth: 1 });
+    },
+  });
+
+  await crawler.run([testVoiceActorUrl]);
+}
