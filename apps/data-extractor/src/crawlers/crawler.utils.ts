4334378add786ef78076473f19d8543e1b62abe3
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2024-03-23 10:32:56 +0100

    feat: adjust crawler settings to ensure scalability

--- apps/data-extractor/src/crawlers/crawler.utils.ts
+++ apps/data-extractor/src/crawlers/crawler.utils.ts
@@ -1,14 +1,16 @@
 import { log, CheerioCrawlingContext, CheerioCrawlerOptions } from 'crawlee'
 
+export function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export const DEFAULT_CRAWLER_OPTIONS: CheerioCrawlerOptions = {
   minConcurrency: 2,
-  maxConcurrency: 10,
+  maxConcurrency: 5,
   maxRequestRetries: 3,
   requestHandlerTimeoutSecs: 30,
   maxRequestsPerMinute: 250,
   retryOnBlocked: true,
-  sameDomainDelaySecs: 0.5,
-  forceResponseEncoding: "iso-8859-1",
   failedRequestHandler({ request }: CheerioCrawlingContext) {
     log.error(`Request ${request.url} failed three times.`)
   },
